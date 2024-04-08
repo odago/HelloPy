7a4b1b3fab253e960fc4e79fd221de22a8829d0a
Francis Asembo <45119417+odago@users.noreply.github.com>
2024-04-08 19:44:41 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,15 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 1+1
+2
+>>> name = input("Enter your name: ")
+Enter your name: jacity
+>>> age = input("Enter your age: ")
+Enter your age: 27
+>>> location = input("Where are you located? ")
+Where are you located?  
+>>> 
+>>> location = input("Where are you located? ")
+Where are you located? Kanairo
+>>> print("Hello " + name + ", you are " + age + " years old and locate in " + location)
+Hello jacity, you are 27 years old and locate in Kanairo
